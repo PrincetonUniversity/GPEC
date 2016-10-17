e50e1ccf09a25b14139b09ec0bb21d7c7ec87cd7
N.C. Logan <nlogan@pppl.gov>
2016-10-17 16:05:39 -0400

    GPEC - Bug Fix - Fixed normalization of filter_flag filtering.
      - Filtering vector normalized by sqrt(A) scalar to match the new normalized field (T) convention used to form the matrices.
      - Similar fix of the vector used to calculate the netcdf overlap output.

--- equil/global.f
+++ equil/global.f
@@ -14,7 +14,7 @@ c-----------------------------------------------------------------------
       USE cspline_mod
       IMPLICIT NONE
 
-      CHARACTER(32), PARAMETER :: version = "GPEC version 1.0.4"
+      CHARACTER(32), PARAMETER :: version = "GPEC version 1.0.5"
 
       CHARACTER(16) :: eq_type="fluxgrid"
       CHARACTER(128) :: eq_filename="fluxgrid.dat"
