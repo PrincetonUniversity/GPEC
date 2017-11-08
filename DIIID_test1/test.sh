da90fe3c3bdbae44469129904e2a54c6f0dcc484
N.C. Logan <nlogan@pppl.gov>
2017-11-07 19:52:05 -0500

    Removed spurious example from top level directory.
    Use the official examples in docs/examples!

--- DIIID_test1/test.sh
+++
@@ -1,9 +0,0 @@
-#cd /p/gpec/users/syang/testcase1/gpec
-#make gpec
-#cd /u/syang/jpartest
-
-/p/gpec/users/syang/testcase1/gpec/gpec
-#sed 's/ //g' test1.dat >test2.dat
-#sed 's/com/ com/g' test2.dat > test.dat
-#matlab -nodesktop -nodisplay plot_jpar.m
-#matlab -nojvm < plot_jpar.m
