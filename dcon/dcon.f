4fd74f204895b2aace7769f5baa154681a3f492d
N.C. Logan <nlogan@pppl.gov>
2018-03-10 21:56:36 -0500

    DCON - Update - Adds ability to move ODE start of integration by specifying qlow

--- dcon/dcon.f
+++ dcon/dcon.f
@@ -55,7 +55,7 @@ c-----------------------------------------------------------------------
      $     cyl_flag,dmlim,lim_flag,sas_flag,sing_order,sort_type,
      $     termbycross_flag,qhigh,kin_flag,con_flag,kinfac1,kinfac2,
      $     kingridtype,ktanh_flag,passing_flag,use_classic_splines,
-     $     ion_flag,electron_flag,ktc,ktw,parallel_threads
+     $     ion_flag,electron_flag,ktc,ktw,parallel_threads,qlow
       NAMELIST/dcon_output/interp,crit_break,out_bal1,
      $     bin_bal1,out_bal2,bin_bal2,out_metric,bin_metric,out_fmat,
      $     bin_fmat,out_gmat,bin_gmat,out_kmat,bin_kmat,out_sol,
