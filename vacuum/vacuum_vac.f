78c338c2afd474c00584543458bac57fc13db202
Aaron Froese <15334145+AaronFroese@users.noreply.github.com>
2019-03-13 16:43:39 -0700

    Removed duplicate specification of arbitrary wall (ishape=9 instead of ishape=41).

--- vacuum/vacuum_vac.f
+++ vacuum/vacuum_vac.f
@@ -1321,9 +1321,6 @@ c     follow nth=nth0+5  nth0=mth  mth2=mth+2 mth1=mth+1=npots0
       if ( ishape .ne. 8 ) go to 349
       call d3dwall ( xwal1, zwal1, mth, outmod, iotty )
  349  continue
-      if ( ishape .ne. 9 ) go to 351
-      call wallfromfile ( xwal1, zwal1, mth, outmod, iotty )
- 351  continue
       if ( ishape .ne. 11 ) go to 400
       do 350 i = 1, mth2
       the = (i-1) * dth
@@ -2319,40 +2316,3 @@ c-----------------------------------------------------------------------
       return
  999  call errmes ( outpest, 'wtopest' )
       end subroutine wtopest
-c-----------------------------------------------------------------------
-c     subprogram 23. wallfromfile.
-c     defines the shape of an arbitrary conductor from a file
-c-----------------------------------------------------------------------
-c-----------------------------------------------------------------------
-c     declarations.
-c-----------------------------------------------------------------------
-      subroutine wallfromfile ( xwall, zwall, mthh, iomod, iotty1 )
-      USE vglobal_mod
-      IMPLICIT REAL*8 (a-h,o-z)
-
-      integer ierr
-      dimension xwall(*), zwall(*), xin(1000), zin(1000)
-c-----------------------------------------------------------------------
-c     computations.
-c-----------------------------------------------------------------------
-      
-c-----------------------------------------------------------------------
-c     read scalars.
-c-----------------------------------------------------------------------
-      open(100, file='vacwall.in' )
-      read(100,*) mthin
-      read(100,*) (xin(ith),zin(ith),ith=1,mthin)
-      close(100)
-      
-      if( mthin .ne. mthh ) then
-         write(iomod, '("wallfromfile: mthin/=mthh",2i6)') mthin,mthh
-         write(iotty1,'("wallfromfile: mthin/=mthh",2i6)') mthin,mthh
-      endif
-
-      xwall(mthh+1) = xwall(1)
-      zwall(mthh+1) = zwall(1)
-c-----------------------------------------------------------------------
-c     termination.
-c-----------------------------------------------------------------------
-      return
-      end subroutine wallfromfile
