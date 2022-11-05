2367534b28f5832f8fcc193dcd52f6525b1dd14a
logan-nc <logan35@llnl.gov>
2022-11-04 17:01:39 -0700

    Enables shifts and tilts of coils directly within GPEC

--- coil/coil.f
+++ coil/coil.f
@@ -25,6 +25,9 @@ c-----------------------------------------------------------------------
       CHARACTER(24) :: machine,ceq_type,ip_direction,bt_direction
       CHARACTER(24), DIMENSION(50) :: coil_name
       REAL(r8), DIMENSION(50,48) :: coil_cur
+      REAL(r8), DIMENSION(50,48) :: coil_shiftx,coil_shifty,coil_shiftz
+      REAL(r8), DIMENSION(50,48) :: coil_tiltx,coil_tilty,coil_tiltz
+      REAL(r8), DIMENSION(50,48) :: coil_xnom,coil_ynom,coil_znom
       REAL(r8) :: cro,czo,cpsio,cpsilow,cpsilim,cqlim,
      $     ipd,btd,helicity
 
@@ -62,11 +65,17 @@ c-----------------------------------------------------------------------
 
       NAMELIST/coil_control/ceq_type,cmpsi,cmtheta,cmzeta,cmlow,cmhigh,
      $     data_dir,machine,ip_direction,bt_direction,
-     $     coil_num,coil_name,coil_cur
+     $     coil_num,coil_name,coil_cur,
+     $     coil_shiftx, coil_shifty, coil_shiftz,
+     $     coil_tiltx, coil_tilty, coil_tiltz,
+     $     coil_xnom, coil_ynom, coil_znom
       NAMELIST/coil_output/gpec_interface
 
-      INTEGER :: ci,cj,ck,cl,cm,ci1,ci2,ci3,ci4
+      INTEGER :: ci,cj,ck,cl,cm,ci1,ci2,ci3,ci4,nsec
       REAL(r8) :: cr1,cr2
+      REAL(r8) :: x0, y0, z0, x, y, z, r, angle, length
+      REAL(r8) :: dtor = pi/180
+      REAL(r8), DIMENSION(:),ALLOCATABLE :: dl, xm, ym, zm
 c-----------------------------------------------------------------------
 c     initialize and read input data.
 c-----------------------------------------------------------------------
@@ -76,6 +85,12 @@ c-----------------------------------------------------------------------
       cmlow=-64
       cmhigh=64
       coil_cur=0
+      coil_shiftx=0
+      coil_shifty=0
+      coil_shiftz=0
+      coil_tiltx=0
+      coil_tilty=0
+      coil_tiltz=0
       CALL ascii_open(in_unit,"coil.in","OLD")
       READ(UNIT=in_unit,NML=coil_control)
       READ(UNIT=in_unit,NML=coil_output)
@@ -94,23 +109,73 @@ c     read coils for each machine.
 c-----------------------------------------------------------------------
       IF(coil_num>0)ALLOCATE(coil(coil_num))
 
-      DO ci=1,coil_num
+      DO ci=1, coil_num
          cfile=TRIM(data_dir)//"/"//TRIM(machine)//"_"//
      $         TRIM(coil_name(ci))//".dat"
-         CALL ascii_open(coil_unit,cfile,"old")
+         CALL ascii_open(coil_unit, cfile, "old")
          coil(ci)%coil_name=TRIM(coil_name(ci))
-         READ(coil_unit, *)
-     $        coil(ci)%ncoil,coil(ci)%s,coil(ci)%nsec,coil(ci)%nw
+         READ(coil_unit,  *)
+     $        coil(ci)%ncoil, coil(ci)%s, nsec, coil(ci)%nw
+         nsec = nsec
          ALLOCATE(coil(ci)%cur(coil(ci)%ncoil))
-         ALLOCATE(coil(ci)%x(coil(ci)%ncoil,coil(ci)%s,coil(ci)%nsec),
-     $        coil(ci)%y(coil(ci)%ncoil,coil(ci)%s,coil(ci)%nsec),
-     $        coil(ci)%z(coil(ci)%ncoil,coil(ci)%s,coil(ci)%nsec))
-         DO cj=1,coil(ci)%ncoil
-            coil(ci)%cur(cj)=coil_cur(ci,cj)
-            DO ck=1,coil(ci)%s
-               DO cl=1,coil(ci)%nsec
-                  READ(coil_unit,*)coil(ci)%x(cj,ck,cl),
-     $                 coil(ci)%y(cj,ck,cl),coil(ci)%z(cj,ck,cl)
+         ALLOCATE(coil(ci)%x(coil(ci)%ncoil, coil(ci)%s, nsec),
+     $        coil(ci)%y(coil(ci)%ncoil, coil(ci)%s, nsec), 
+     $        coil(ci)%z(coil(ci)%ncoil, coil(ci)%s, nsec))
+        ALLOCATE(dl(nsec), xm(nsec), ym(nsec), zm(nsec))
+         DO cj=1, coil(ci)%ncoil
+            coil(ci)%cur(cj)=coil_cur(ci, cj)
+            DO ck=1, coil(ci)%s
+               DO cl=1, nsec
+                  READ(coil_unit, *) coil(ci)%x(cj, ck, cl),
+     $                 coil(ci)%y(cj, ck, cl), coil(ci)%z(cj, ck, cl)
+               ENDDO
+            ENDDO
+            ! apply shifts
+            coil(ci)%x(cj,:,:) = coil(ci)%x(cj,:,:) + coil_shiftx(ci,cj)
+            coil(ci)%y(cj,:,:) = coil(ci)%y(cj,:,:) + coil_shifty(ci,cj)
+            coil(ci)%z(cj,:,:) = coil(ci)%z(cj,:,:) + coil_shiftz(ci,cj)
+            ! center of mass is default coil center
+            x0 = coil_xnom(ci, cj)
+            y0 = coil_ynom(ci, cj)
+            z0 = coil_znom(ci, cj)
+            DO ck=1, coil(ci)%s
+               dl = sqrt( (coil(ci)%x(cj, ck, 2:nsec)
+     $                     - coil(ci)%x(cj, ck, 1:nsec-1)) ** 2
+     $                   + (coil(ci)%y(cj, ck, 2:nsec)
+     $                      - coil(ci)%y(cj, ck, 1:nsec-1)) ** 2
+     $                   + (coil(ci)%z(cj, ck, 2:nsec)
+     $                      - coil(ci)%z(cj, ck, 1:nsec-1)) ** 2
+     $                  )
+               length = sum(dl)
+               xm = (coil(ci)%x(cj, ck, 2:nsec)
+     $               + coil(ci)%x(cj, ck, 1:nsec-1))/2
+               ym = (coil(ci)%y(cj, ck, 2:nsec)
+     $               + coil(ci)%y(cj, ck, 1:nsec-1))/2
+               zm = (coil(ci)%z(cj, ck, 2:nsec)
+     $               + coil(ci)%z(cj, ck, 1:nsec-1))/2
+               IF(ABS(x0) > 1e3) x0 = sum(xm * dl) / length
+               IF(ABS(y0) > 1e3) y0 = sum(ym * dl) / length
+               IF(ABS(z0) > 1e3) z0 = sum(zm * dl) / length
+               ! apply tilts around coil center (user supplies this in degrees)
+               DO cl=1, nsec
+                    x = coil(ci)%x(cj, ck, cl) - x0
+                    y = coil(ci)%y(cj, ck, cl) - y0
+                    z = coil(ci)%z(cj, ck, cl) - z0
+                    ! ztilt (clocking toroidally around the axis)
+                    r = sqrt( y ** 2 + x ** 2)
+                    angle = ATAN2(y, x) + coil_tiltz(ci, cj) * dtor
+                    coil(ci)%x(cj, ck, cl) = x0 + r * cos(angle)
+                    coil(ci)%y(cj, ck, cl) = y0 + r * sin(angle)
+                    ! ytilt (rotating around the y axis)
+                    r = sqrt( x ** 2 + z ** 2)
+                    angle = ATAN2(x, z) + coil_tiltz(ci, cj) * dtor
+                    coil(ci)%z(cj, ck, cl) = z0 + r * cos(angle)
+                    coil(ci)%x(cj, ck, cl) = x0 + r * sin(angle)
+                    ! xtilt (rotating around the x axis)
+                    r = sqrt( z ** 2 + y ** 2)
+                    angle = ATAN2(z, y) + coil_tiltx(ci, cj) * dtor
+                    coil(ci)%y(cj, ck, cl) = y0 + r * cos(angle)
+                    coil(ci)%z(cj, ck, cl) = z0 + r * sin(angle)
                ENDDO
             ENDDO
          ENDDO
