66ee8c7d5e99425516cd32a9bb0a10bf11b955d3
Christopher Albert <albert@alumni.tugraz.at>
2024-11-08 16:30:37 +0000

    DEBUG

--- install/debug_gnu.sh
+++ install/debug_gnu.sh
@@ -3,7 +3,9 @@
 export FC=gfortran
 export CC=gcc
 export CXX=g++
-export FFLAGS="-g -O0 -fbacktrace -fdefault-real-8 -fdefault-double-8 -fallow-argument-mismatch -lnetcdf -lnetcdff"
+export FFLAGS="-g -O0 -fbacktrace -fdefault-real-8 -fdefault-double-8 \
+    -fallow-argument-mismatch -lnetcdf -lnetcdff \
+    -finit-real=nan"
 export NETCDF_FORTRAN_HOME=/usr/include
 export LAPACK_HOME=/usr/include
-OMPFLAG=-fopenmp make NETCDFLIBS="-lnetcdf -lnetcdff"
+OMPFLAG="-fopenmp" make NETCDFLIBS="-lnetcdf -lnetcdff"
