db33b8f31041828cc07b8b184a95b21386b02131
Christopher Albert <albert@alumni.tugraz.at>
2024-11-08 10:39:31 +0000

    Debug GNU

---
+++ install/debug_gnu.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export FC=gfortran
+export CC=gcc
+export CXX=g++
+export FFLAGS="-g -O0 -fbacktrace -fdefault-real-8 -fdefault-double-8 -fallow-argument-mismatch -lnetcdf -lnetcdff"
+export NETCDF_FORTRAN_HOME=/usr/include
+export LAPACK_HOME=/usr/include
+OMPFLAG=-fopenmp make NETCDFLIBS="-lnetcdf -lnetcdff"
