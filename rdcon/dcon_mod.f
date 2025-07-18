7133673783f1db4ec352ba85d253411f29d895a5
StuartBenjamin <stubenj9@mit.edu>
2025-07-18 18:16:54 -0400

    adding Zeffective namelist object - necessary input for MRE terms

--- rdcon/dcon_mod.f
+++ rdcon/dcon_mod.f
@@ -81,7 +81,7 @@ c-----------------------------------------------------------------------
      $     sing_start=0,nzero=0
       REAL(r8) :: thmax0=1,ucrit=1e4,tol_r=1e-5,tol_nr=1e-5,
      $     crossover=1e-2,mthsurf0=1,prefac=1.0,
-     $     plasma1=0.0,vacuum1=0.0,total1=0.0
+     $     plasma1=0.0,vacuum1=0.0,total1=0.0,Zeff=1.52
 
       REAL(r8) :: dpsi_intvl=0.1,dpsi1_intvl=0.1
       TYPE(spline_type) :: locstab, mreterms
