bf64d65380ae8f6d3f1a020a32d847b4365e116e
StuartBenjamin <stubenj9@mit.edu>
2025-07-18 18:19:44 -0400

    Initialising new parameters for MRE term computation

--- rdcon/mercier.f
+++ rdcon/mercier.f
@@ -30,7 +30,9 @@ c-----------------------------------------------------------------------
 
       INTEGER :: ipsi,itheta
       REAL(r8) :: bsq,chi1,di,dpsisq,eta,h,jac,p1,psifac,q,q1,r,
-     $     rfac,term,theta,twopif,v1,v2,v21,v22,v23,v33
+     $     rfac,term,theta,twopif,v1,v2,v21,v22,v23,v33,
+     $     bt,f1,Dnc,Dnc_prefac,Wc_prefac,eps_loc,Jpara,ftr,Mloc,
+     $     Hbs_prefac,Jboot_dot_B,mufrac,taua_prefac,taur_prefac
       REAL(r8), DIMENSION(:), POINTER :: avg
       TYPE(spline_type), TARGET :: ff
 c-----------------------------------------------------------------------
