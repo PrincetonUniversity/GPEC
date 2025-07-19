1a032a41e8319246a78dd674ea7de53f3ddd46f1
StuartBenjamin <stubenj9@mit.edu>
2025-07-19 17:50:21 -0400

    reforming equilibrium based new qhigh, qlow and sasflag limits

--- rdcon/dcon.f
+++ rdcon/dcon.f
@@ -280,6 +280,13 @@ c-----------------------------------------------------------------------
       IF (reform_eq_with_psilim .OR. qlow > sq%xs(0)) THEN
          CALL sing_lim  ! determine if qhigh is truncating before psihigh
          CALL sing_min
+         IF(psilim /= psihigh .OR. psilow /= sq%xs(0))THEN
+            psilow_tmp = psilow  ! if we feed psilow directly, it get's overwritten by namelist read
+            psilim_tmp = psilim
+            CALL equil_read(out_unit, psilim_tmp, psilow_tmp)
+            CALL equil_out_global
+            CALL equil_out_qfind
+         ENDIF
       ENDIF
 c-----------------------------------------------------------------------
 c     define poloidal mode numbers.
